693c66469bc3c7339cf2723bdd213c0644bfdf0d
Alex <alex.fallen.part@gmail.com>
2018-03-04 09:37:36 +0300

    function added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var br=[];
+
+    for (var i=0; i<bracketsConfig.length;i++)
+    {
+        arr=bracketsConfig[i];
+        br[i]= arr[0]+ arr[1];
+    }
+
+    for ( i=0; i<br.length;i++)
+    {
+        var pos=0;
+
+        if (str.indexOf(br[i])>=0) // нашли какие-нибудь скобки
+        {
+
+            pos=str.indexOf(br[i]);
+            str=str.slice(0,pos)+str.slice(pos+2)
+
+            i=-1;
+
+        }
+
+
+    }
+    if (str.length===0) {return true;}
+    else {return false;}
 }
